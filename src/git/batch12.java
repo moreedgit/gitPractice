95aabd043fe261bb1e41fcedbef4a705053f116d
moreedgit <moreeds4@gmail.com>
2022-02-16 18:52:25 -0800

    the commit was easy

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -5,6 +5,9 @@ public class batch12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("we are we");
+System.out.println("we are");
+System.out.println("we sdfssdfsdfsdfs");
+
 	}
 
 }
